ab35c6a10764171efa6801e18fce2c8d6dc14d67
Adedero <118962395+Adedero@users.noreply.github.com>
2024-01-13 08:39:33 +0100

    completed the mobile view

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -2,24 +2,28 @@
 import FbIcon from '@/components/FbIcon.vue';
 import GhIcon from '@/components/GhIcon.vue';
 import InIcon from '@/components/InIcon.vue';
+import 'animate.css'
 </script>
 
 <template>
     <div class="h-full flex flex-col justify-between md:justify-center md:w-1/2">
-        <h1 class="hero-text relative text-[4rem] font-extrabold md:text-5[rem] lg:text-[5.5rem] animate__animated animate__fadeInDown amimate__fast">Hi, <br> I
-            <span class="absolute top-10 left-5 lg:top-10 lg:left-[1.7rem]">,</span>
+        <h1 class="hero-text text-[4rem] text-center font-extrabold md:hidden animate__animated animate__fadeInDown amimate__fast">Hi, <br> I'm Dero
+        </h1>
+
+        <h1 class="hidden hero-text relative text-[4rem] font-extrabold text-5[rem] md:block lg:text-[5.5rem] animate__animated animate__fadeInDown amimate__fast">Hi, <br> I
+            <span class="absolute top-10 left-[1.25rem] lg:top-10 lg:left-[1.7rem]">,</span>
             m Dero
         </h1>
 
-        <div class="img-container w-[16rem] relative md:hidden animate__animated animate__fadeInRight animate__fast animate__delay-1s">
-            <img class="my-pic w-72 z-10 rounded-xl relative dark:filter dark:grayscale" src="../assets/images/my_pic.jpg" alt="adedero_pic">
+        <div class="img-container w-[16rem] mx-auto relative md:hidden animate__animated animate__fadeInRight animate__fast animate__delay-1s">
+            <img class="my-pic w-72 h-72 object-cover z-10 rounded-xl relative dark:filter dark:grayscale" src="../assets/images/me.png" alt="adedero_pic">
         </div>
 
-        <p class="desc text-xl dark:font-[300] pt-8 pr-24 md:pr-0 md:text-2xl lg:pr-24 animate__animated animate__fadeInUp amimate__fast animate__delay-1s">
-            Front end developer dedicated to making your designs a picture perfect reality. If you can think it, I can build it!
+        <p class="desc text-xl dark:font-[300] pt-8 text-center px-5 md:text-left md:px-0 md:text-2xl lg:pr-24 animate__animated animate__fadeInUp amimate__fast animate__delay-1s">
+            Front end developer dedicated to making your designs a picture perfect reality. <br> If you can think it, I can build it!
         </p>
 
-        <div class="flex items-center gap-6 pt-2 md:pt-11 lg:pt-12 animate__animated animate__fadeInUp amimate__fast animate__delay-1s">
+        <div class="flex items-center justify-center gap-6 pt-2 md:justify-start md:pt-11 lg:pt-12 animate__animated animate__fadeInUp amimate__fast animate__delay-1s">
             <a href="#">
                 <FbIcon />
             </a>
@@ -33,8 +37,8 @@ import InIcon from '@/components/InIcon.vue';
     </div>
 
     <div class="hidden md:grid md:justify-end md:pr-10 md:w-1/2">
-        <div class="img-container w-72 lg:w-[80%] justify-self-end relative mr-5">
-            <img class="my-pic w-72 lg:w-[90%] lg:h-[30rem] lg:object-cover z-10 rounded-xl relative dark:filter dark:grayscale" src="../assets/images/my_pic.jpg" alt="adedero_pic">
+        <div class="img-container w-72 lg:w-[80%] justify-self-end relative mr-5 animate__animated animate__zoomIn">
+            <img class="my-pic-2 w-72 lg:w-[90%] lg:h-[30rem] lg:object-cover z-10 rounded-xl relative dark:filter dark:grayscale animate__animated animate__fadeInUp amimate__fast animate__delay-1s" src="../assets/images/me.png" alt="adedero_pic">
         </div>
     </div>
 </template>
@@ -54,6 +58,7 @@ import InIcon from '@/components/InIcon.vue';
 .img-container::after {
     @apply hidden content-[""] absolute w-32 h-32 border-[0.5px] border-[var(--dark)] -right-8 -bottom-8 rounded-full dark:border-[var(--light)] md:block lg:-bottom-8 lg:right-[0%] lg:w-40 lg:h-40
 }
+
 
 @media screen and (min-width: 1024px) {
     .hero-text {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import HeroView from './HeroView.vue';
 </script>
 
 <template>
-    <section id="home" class="overflow-y-scroll pt-16 h-[100dvh] bg-inherit pb-5 px-5 md:px-10 md:flex md:items-center md:justify-between fixed w-full top-0 z-[-1]">
+    <section id="home" class="pt-16 h-[100dvh] bg-inherit pb-5 px-5 md:px-10 md:flex md:items-center md:justify-between fixed w-full top-0 z-[-1] overflow-y-scroll ">
         <HeroView />
     </section>
     <section id="about" class="h-[100dvh] mt-[100dvh] z-[999] bg-red-500"></section>
